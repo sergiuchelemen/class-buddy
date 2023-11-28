6036eb43a7338d5f3d2a545a3b21e67055820f0d
Sergiu Chelemen <schelemen90@gmail.com>
2023-11-29 01:09:16 +0200

    first commit

---
+++ src/main/java/com/demo/ClassBuddy/ClassBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.demo.ClassBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/ClassBuddy/ClassBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.ClassBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
