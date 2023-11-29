91de63b9c1d650747a2ba113c5e232f6495f530e
Purice Victor <pvictor0110@gmail.com>
2023-11-29 19:20:29 +0200

    Описание ваших изменений здесь

---
+++ client/src/App.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+interface Data {
+  message: string;
+}
+
+function App() {
+  const url = "http://localhost:8080/";
+
+  const [data, setData] = useState<Data | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (response.ok) {
+          const result: Data = await response.json();
+          setData(result);
+        } else {
+          throw new Error("Failed to fetch data");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [url]); 
+
+  return (
+    <div className="app">
+      <div>{data ? data.message : "Loading..."}</div>
+    </div>
+  );
+}
+
+export default App;
--- src/main/java/com/demo/ClassBuddy/Controller.java
+++ src/main/java/com/demo/ClassBuddy/Controller.java
@@ -1,15 +1,35 @@
 package com.demo.ClassBuddy;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping(path = "/")
+@CrossOrigin(origins = "http://localhost:5173/")
+
+
 public class Controller {
 
     @GetMapping
-    public String hello(){
-        return "Hello World!";
+    @ResponseBody
+    public MyResponse hello(){
+        MyResponse response = new MyResponse();
+        response.setMessage("Hello World!");
+        return response;
+    }
+
+    public static class MyResponse {
+        private String message;
+
+        // Constructor, getter și setter pentru 'message'
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
     }
 }
+
