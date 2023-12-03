7d17c2de16cee206d5d66cdf06cbe339688b20c8
pvictordev <pvictor0110@gmail.com>
2023-12-03 13:51:14 +0200

    added register test

--- client/src/test/Register.test.tsx
+++ client/src/test/Register.test.tsx
@@ -1,22 +1,41 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import Register from "../pages/register";
+import { render, fireEvent } from "@testing-library/react";
+import RegistrationForm from "../pages/Register";
 
-const isFieldEmpty = (fieldName: string) => {
-  return emptyFields.includes(fieldName);
-};
-const emptyFields = ["field1", "field2", "field3"];
+describe("RegistrationForm component", () => {
+  test("form submission with valid data", () => {
+    const { getByPlaceholderText, getByText } = render(<RegistrationForm />);
 
-describe("isFieldEmpty", () => {
-  it("should return true if the field is empty", () => {
-    expect(isFieldEmpty("field1")).toBe(true);
+    // Fill in the form fields
+    fireEvent.change(getByPlaceholderText("First Name"), {
+      target: { value: "John" },
+    });
+    fireEvent.change(getByPlaceholderText("Last Name"), {
+      target: { value: "Doe" },
+    });
+    fireEvent.change(getByPlaceholderText("Username"), {
+      target: { value: "johndoe123" },
+    });
+    fireEvent.change(getByPlaceholderText("Email"), {
+      target: { value: "john@example.com" },
+    });
+    fireEvent.change(getByPlaceholderText("Password"), {
+      target: { value: "password123" },
+    });
+    fireEvent.change(getByPlaceholderText("Confirm Password"), {
+      target: { value: "password123" },
+    });
+    fireEvent.change(getByPlaceholderText("Date of Birth"), {
+      target: { value: "1990-01-01" },
+    });
+    fireEvent.change(getByPlaceholderText("Select Student Status"), {
+      target: { value: "Bachelor's Degree" },
+    });
+
+    // Submit the form
+    fireEvent.click(getByText("Register"));
+    
+    expect(getByText("Registration Successful")).toBeTruthy();
   });
 
-  it("should return false if the field is not empty", () => {
-    expect(isFieldEmpty("field4")).toBe(false);
-  });
-
-  it("should handle empty field name", () => {
-    expect(isFieldEmpty("")).toBe(false);
-  });
 });
