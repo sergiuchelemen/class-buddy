f59d4cf7d50a154a11e19001c132d762ef32a1eb
pvictordev <pvictor0110@gmail.com>
2023-12-04 23:57:04 +0200

    cors

--- src/main/java/com/demo/ClassBuddy/Controller.java
+++ src/main/java/com/demo/ClassBuddy/Controller.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/")
+@CrossOrigin(origins = "http://localhost:5173/")
+
 public class Controller {
     private final AuthenticationService authenticationService;
 
