bf77a0746160971b1af5c58b82b5b20bc46e6d1c
Sergiu Chelemen <schelemen90@gmail.com>
2024-04-04 00:09:56 +0300

    added auth entry point

--- src/main/java/com/demo/ClassBuddy/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/demo/ClassBuddy/security/CustomAuthenticationEntryPoint.java
@@ -1,39 +1,24 @@
 package com.demo.ClassBuddy.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
-import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 
 @Component
-@AllArgsConstructor
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final HandlerExceptionResolver resolver;
+
+    public CustomAuthenticationEntryPoint(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
+        this.resolver = resolver;
+    }
+
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-
-        Map<String, String> errorResponse = new HashMap<>();
-        errorResponse.put("message", "Token is missing, invalid or expired.");
-        errorResponse.put("httpStatus", "Unauthorized");
-        errorResponse.put("timestamp", String.valueOf(Timestamp.valueOf(LocalDateTime.now())));
-        errorResponse.put("redirectTo", "/login");
-
-        PrintWriter writer = response.getWriter();
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.writeValue(writer, errorResponse);
-        writer.flush();
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) {
+        this.resolver.resolveException(request, response, null, authException);
     }
 }
