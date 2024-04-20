fb7759ff6dd415c28b69a51f4c4715ab674adf0b
Sergiu Chelemen <schelemen90@gmail.com>
2024-04-21 01:23:34 +0300

    added test to check application context

--- src/test/java/com/demo/ClassBuddy/ClassBuddyApplicationTests.java
+++ src/test/java/com/demo/ClassBuddy/ClassBuddyApplicationTests.java
@@ -1,13 +1,20 @@
 package com.demo.ClassBuddy;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class ClassBuddyApplicationTests {
 
+	@Autowired
+	ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(applicationContext);
 	}
-
 }
