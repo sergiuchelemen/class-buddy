d86ddc543e1b60a1a066ec49d3b0a7e81b41fc58
Sergiu Chelemen <schelemen90@gmail.com>
2024-04-21 01:15:05 +0300

    added dynamic properties to classroom repo

--- src/test/java/com/demo/ClassBuddy/repository/ClassroomRepositoryTest.java
+++ src/test/java/com/demo/ClassBuddy/repository/ClassroomRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -24,6 +26,13 @@ class ClassroomRepositoryTest {
 
     @Autowired
     ClassroomRepository classroomRepository;
+
+    @DynamicPropertySource
+    static void dynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+    }
 
     @BeforeEach
     public void setup() {
